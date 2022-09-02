5e9c7cfb40cbb025df47b9312e19b2e9a58cf976
3axapuo <3axapuo@mail.ru>
2022-09-03 01:05:28 +0300

    Completed task #1.
    Удалил класс компаратора и в методе main заменил объект этого удалённого класса на лямбду с той же логикой. Т.е. имплементация компаратора на лету с помощью лямбды.
    
    использовал напрямую в коде лямбды то значение (maxSurnameWords) , которое раньше передавали в конструктор компаратора.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,29 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        final int maxSurnameWords = 7; // максимальное число слов в фамилии
         List<Person> clients = generateClients();
-        Collections.sort(clients, new SurnameCalculate(7).reversed());
+
+        //ЗАДАЧА №1, Удалите класс компаратора и в методе main замените объект этого удалённого класса
+        //на лямбду с той же логикой. Т.е. по сути мы дадим имплементацию компаратору на лету с помощью лямбды.
+        //Если вы делали вторую задачу, то параметра не будет (их у лямбд не бывает) - вместо этого просто
+        //используйте напрямую в коде лямбды то значение, которое раньше передавали в конструктор компаратора.
+        clients.sort((o1, o2) -> {
+            int lengthSurnameWordsPerson_1 = o1.getSurname().split(" ").length;
+            int lengthSurnameWordsPerson_2 = o2.getSurname().split(" ").length;
+            int lengthDifferenceOfWords = lengthSurnameWordsPerson_1 - lengthSurnameWordsPerson_2;
+
+            if ((lengthSurnameWordsPerson_1 >= maxSurnameWords &&
+                    lengthSurnameWordsPerson_2 >= maxSurnameWords)
+                    || lengthDifferenceOfWords == 0) {
+                if (o1.getAge() > o2.getAge()) lengthDifferenceOfWords = 1;
+                if (o1.getAge() < o2.getAge()) lengthDifferenceOfWords = -1;
+            }
+            return lengthDifferenceOfWords;
+        });
         System.out.println(clients);
     }
-
     public static List<Person> generateClients() {
         List<Person> clients = new ArrayList<>();
         clients.add(new Person("Билл", "Гейтс", 5));
